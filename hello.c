985ece465396fa85ca03caedb74ea0c1283e3386
himaja-poluri <68316936+himaja-poluri@users.noreply.github.com>
2023-02-11 22:02:35 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2016 ilbers GmbH
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <hello.h>
+
+int main(void)
+{
+	say_hello();
+	return 0;
+}
